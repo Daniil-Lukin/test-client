3f91707f8caa1a9c4245d91a31e3e35788f53fcb
Daniil Lukin <hospodi322@gmail.com>
2023-07-24 17:10:01 +0300

    upd for dep

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: `https://lab-server-0c87e2b3c25d.herokuapp.com/api`,
-  apiUrl: `lab-server-ts-1da2385f90b1.herokuapp.com/api`,
+  // apiUrl: `https://lab-server-0c87e2b3c25d.herokuapp.com/api`, past
+  apiUrl: `https://lab-server-ts-1da2385f90b1.herokuapp.com/api`,
 };
